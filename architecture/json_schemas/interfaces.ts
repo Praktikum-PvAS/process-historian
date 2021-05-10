5ae5513adb32215647526c38627dd548fa52eddb
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-05-10 14:50:42 +0200

    Add json schemas

---
+++ architecture/json_schemas/interfaces.ts
@@ -0,0 +1,61 @@
+export interface OPCUAConfig {
+    host: string;
+
+    sensors: {
+        id: string;
+        nodeId: string;
+        namespace: string;
+        attributes: {
+            name: string;
+            nodeId: string;
+            namespace: string;
+            mode: "poll" | "subscription";
+            intervall: number;
+        }[];
+    }[];
+
+    actuators: {
+        id: string;
+        nodeId: string;
+        namespace: string;
+        attributes: {
+            name: string;
+            nodeId: string;
+            namespace: string;
+            mode: "poll" | "subscription";
+            intervall: number;
+        }[];
+    }[];
+
+    services: {
+        id: string;
+        nodeId: string;
+        namespace: string;
+        attributes: {
+            name: string;
+            nodeId: string;
+            namespace: string;
+            mode: "poll" | "subscription";
+            intervall: number;
+        }[];
+    }[];
+}
+
+export interface ProgramConfig {
+    include: ("sensors" | "actuators" | "services")[]
+    tripleStore: {
+        host: string;
+        username: string;
+        password: string;
+    };
+    influxdb: {
+        host: string;
+        username: string;
+        password: string;
+        organization: string;
+        bucket: string;
+    };
+    buffer: {
+        size: number;
+    };
+}
