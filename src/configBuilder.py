da20e231e0861ca83d1bc1d92d84e5124e19a05f
Marc <m.p.becker98@gmail.com>
2021-05-25 17:33:46 +0200

    Add dummie-functions to configBuilder
    Not sure which SPARQL package to use

---
+++ src/configBuilder.py
@@ -0,0 +1,13 @@
+import sparql
+# TODO which sparql to import?
+
+
+class Configurator:
+    def __init__(self, connection_params: dict):
+        self.connection_parameters = connection_params
+
+
+class SPARQLClient:
+    def __init__(self, connection_params: dict):
+        # TODO
+        pass
