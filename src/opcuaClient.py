a313ddba3a9aefa4c75ef39878c417a34fb06d6a
Marc <m.p.becker98@gmail.com>
2021-05-25 17:09:38 +0200

    Add dummie-functions to opcuaclient

---
+++ src/opcuaClient.py
@@ -0,0 +1,35 @@
+import opcua
+import time
+
+
+class Client:
+    def __init__(self, config):
+        self.opcua_config = config
+        url = "unsere URL"  # TODO find out used url / get url out of config?
+        self.opcua_lib_client = opcua.Client(self.opcua_config)
+
+    def create_client(self):
+        pass
+
+    def connect(self):
+        self.opcua_lib_client.connect()
+
+    def disconnect(self):
+        self.opcua_lib_client.disconnect()
+
+    def poll(self):
+        # TODO
+        pass
+
+    def subscribe(self):
+        # TODO
+        pass
+
+
+class SubscriptionHandler:
+    def __init__(self):
+        self.data_callback = callable()
+
+    def datachange_notification(self, node: opcua.Node, value, raw_data):
+        # TODO
+        pass
