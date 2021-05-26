40edda69ce9fdbd4469968ba8b43d495e89d170a
Marc <m.p.becker98@gmail.com>
2021-05-26 17:54:18 +0200

    Rearrange configBuilder
    create package and own files for classes

---
+++ src/configBuilder/configurator.py
@@ -0,0 +1,4 @@
+
+class Configurator:
+    def __init__(self, connection_params: dict):
+        self.connection_parameters = connection_params
--- src/configBuilder/sparqlClient.py
+++ src/configBuilder/sparqlClient.py
@@ -2,11 +2,6 @@ import sparql
 # TODO which sparql to import?
 
 
-class Configurator:
-    def __init__(self, connection_params: dict):
-        self.connection_parameters = connection_params
-
-
 class SPARQLClient:
     def __init__(self, connection_params: dict):
         # TODO
