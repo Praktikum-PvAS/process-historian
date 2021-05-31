e5465ad5e42670108d97dd50a4ffa78b40035d85
patrick <patrick.suwinski@tu-dresden.de>
2021-05-31 21:53:18 +0200

    Add functions to delete points in buffer.py Add connection parameters, defined properties and setter, modified insert function in influxClient.py Add TODO for inserting many points

--- src/cloudBuffer/buffer.py
+++ src/cloudBuffer/buffer.py
@@ -1,9 +1,9 @@
 from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 class Buffer:
-    def __init__(self, connection_params: dict): # ?Buffer needs connection_params?
+    def __init__(self,
+                 connection_params: dict):  # ?Buffer needs connection_params?
         self._buffer = []
 
     @property
@@ -12,7 +12,20 @@ class Buffer:
 
     def append(self, node_name, value, timestamp):
         # TODO what about tags?
-        self._buffer.append(Point(node_name).tag("useful", "tag").field("value", value).time(timestamp))
+        self._buffer.append(
+            Point(node_name).tag("useful", "tag").field("value", value).time(
+                timestamp))
 
-    def pop_first(self):
-        self._buffer.pop(0)
+    def delete_points(self, number_of_element):  # delete points from list
+        self._buffer.remove(number_of_element)
+
+    def clear_list(self):  # clearing all values from list
+        self._buffer.clear()
+
+    def get_number_of_points(self):  # number of points in buffer
+        number_of_points = len(self._buffer)
+        return number_of_points
+
+    def pop_first(self, number_of_element):
+        first_values = self._buffer.pop(number_of_element)
+        return first_values
--- src/cloudBuffer/influxClient.py
+++ src/cloudBuffer/influxClient.py
@@ -1,17 +1,94 @@
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb import SeriesHelper
+from cloudBuffer.buffer import Buffer
 
-
+# TODO send connection_params from buffer ti InfluxClient
 class InfluxClient:  # renamed so not as imported class
     def __init__(self, connection_params: dict):
-        # TODO connection parameters need structure to use
-        self._url = connection_params.url  # TODO Change this later
-        self._token = connection_params.token  # TODO Change this later
-        self._bucket = connection_params.bucket  # TODO Change this later
-        self.influxDBClient = InfluxDBClient(self._url, self._token)
-        self.write_api = self.influxDBClient.write_api(write_options=SYNCHRONOUS)
+        self.url = connection_params['host']
+        self.username = connection_params['username']
+        self.password = connection_params['password']
+        self.org = connection_params['organization']
+        self.bucket = connection_params['bucket']
+
+    # property methods
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def username(self):
+        return self._username
+
+    @property
+    def password(self):
+        return self._password
+
+    @property
+    def org(self):
+        return self._org
+
+    @property
+    def bucket(self):
+        return self._bucket
+
+    # setter methods
+    @url.setter
+    def url(self, url):
+        if url is None:
+            raise ValueError("variable value of url is none")
+        if url == "":
+            raise ValueError("url is empty")
+        self._url = url
+
+    @username.setter
+    def username(self, username):
+        if username is None:
+            raise ValueError("variable value of username is none")
+        if username == "":
+            raise ValueError("username is empty")
+        self._username = username
+
+    @password.setter
+    def password(self, password):
+        if password is None:
+            raise ValueError("variable value of password is none")
+        if password == "":
+            raise ValueError("password is empty")
+        self._password = password
+
+    @org.setter
+    def org(self, org):
+        if org is None:
+            raise ValueError("variable value of org is none")
+        if org == "":
+            raise ValueError("org is empty")
+        self._org = org
+
+    @bucket.setter
+    def bucket(self, bucket):
+        if bucket is None:
+            raise ValueError("variable value of bucket is none")
+        if bucket == "":
+            raise ValueError("bucket is empty")
+        self._bucket = bucket
+
+        # TODO create Token from username and password
+        token = self.username + "" + self.password
+        self.influxDBClient = InfluxDBClient(url=self.url, token=token,
+                                             org=self.org)
+        self.write_api = self.influxDBClient.write_api(
+            write_options=SYNCHRONOUS)
 
     def insert(self, buffer: Buffer):
-        self.write_api.write(bucket=self._bucket, record=buffer)
+        self.write_api.write(bucket=self._bucket,
+                             record=buffer.pop_first(number_of_element=0))
         # TODO check if written successfully
-        # TODO pop written data of buffer
+        buffer.delete_points(number_of_element=0)  # delete single point
+
+    def insert_many(self, buffer: Buffer):
+        # TODO insert many points with MySeriesHelper
+        # TODO https://github.com/influxdata/influxdb-python/blob/master/examples/tutorial_serieshelper.py
+
+        buffer.clear_list()  # delete all points
