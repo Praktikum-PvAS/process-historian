bb726452ddcf22cff1661e4922f2a1cb1a68130c
patrick <patrick.suwinski@tu-dresden.de>
2021-06-02 17:30:15 +0200

    Change insert_many function from influxWrapper

--- src/cloudBuffer/buffer.py
+++ src/cloudBuffer/buffer.py
@@ -13,13 +13,23 @@ class Buffer:
         self.__buffer.append(
             Point(node_name).tag("useful", "tag").field("value", value).time(
                 timestamp))
-
+    # TODO works when other thread will open?
     def write_points(self):
         if not len(self.__buffer) == 1:
             status = self.influxClient.insert(self.__buffer[0])
-            if not status:     # successful
+            if not status:      # successful
                 self.__buffer.remove(0)
-        elif len(self.__buffer) > 1:
+        elif len(self.__buffer) > 1000:
+            __buffer_part = self.__buffer[:1000].copy()
+            status = self.influxClient.insert_many(__buffer_part)
+            if not status:      # successful
+                self.pop_first(len(__buffer_part))
+            else:               # push back when not written
+                self.__buffer.insert(0, __buffer_part)
+        else:
             status = self.influxClient.insert_many(self.__buffer)
-            if not status:     # successful
-                self.__buffer.clear()
+            if not status:  # successful
+                self.pop_first(len(self.__buffer))
+
+    def pop_first(self, number_of_elements):
+        self.__buffer[0:number_of_elements] = []
