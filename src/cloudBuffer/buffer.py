70ada279ba775ef6679b6c4b644d971c2f065a65
patrick <patrick.suwinski@tu-dresden.de>
2021-06-02 12:39:05 +0200

    Add function to insert many points Restructure function to insert signle points Add write points function to add single and multiple points Renamed class influxClient in influxWrapper

--- src/cloudBuffer/buffer.py
+++ src/cloudBuffer/buffer.py
@@ -1,31 +1,25 @@
-from influxdb_client import InfluxDBClient, Point
+from influxdb_client import Point
+from influxWrapper import InfluxWrapper
 
 
 class Buffer:
-    def __init__(self,
-                 connection_params: dict):  # ?Buffer needs connection_params?
-        self._buffer = []
-
-    @property
-    def buffer(self):
-        return self._buffer
+    def __init__(self, connection_params: dict):
+        self.__buffer = []
+        self.influxClient = InfluxWrapper(connection_params)
 
     def append(self, node_name, value, timestamp):
-        # TODO what about tags?
-        self._buffer.append(
+        # TODO structure?
+        # TODO check buffersize
+        self.__buffer.append(
             Point(node_name).tag("useful", "tag").field("value", value).time(
                 timestamp))
 
-    def delete_points(self, number_of_element):  # delete points from list
-        self._buffer.remove(number_of_element)
-
-    def clear_list(self):  # clearing all values from list
-        self._buffer.clear()
-
-    def get_number_of_points(self):  # number of points in buffer
-        number_of_points = len(self._buffer)
-        return number_of_points
-
-    def pop_first(self, number_of_element):
-        first_values = self._buffer.pop(number_of_element)
-        return first_values
+    def write_points(self):
+        if not len(self.__buffer) == 1:
+            status = self.influxClient.insert(self.__buffer[0])
+            if not status:     # successful
+                self.__buffer.remove(0)
+        elif len(self.__buffer) > 1:
+            status = self.influxClient.insert_many(self.__buffer)
+            if not status:     # successful
+                self.__buffer.clear()
