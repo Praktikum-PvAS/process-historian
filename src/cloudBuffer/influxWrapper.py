f9f3bc05ed7d68e825f45a6703b93d239ea70429
patrick <patrick.suwinski@tu-dresden.de>
2021-06-02 16:35:27 +0200

    Change properies of username and password

--- src/cloudBuffer/influxWrapper.py
+++ src/cloudBuffer/influxWrapper.py
@@ -2,16 +2,23 @@ from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS, WriteOptions
 
 
-# TODO send connection_params from buffer ti InfluxClient
 class InfluxWrapper:  # renamed so not as imported class
     def __init__(self, connection_params: dict):
         self.url = connection_params['host']
-        self.username = connection_params['username']
-        self.password = connection_params['password']
+        if connection_params['username'] is None:
+            raise ValueError("variable value of username is none")
+        if connection_params['username'] == "":
+            raise ValueError("username is empty")
+        self.__username = connection_params['username']
+        if connection_params['password'] is None:
+            raise ValueError("variable value of password is none")
+        if connection_params['password'] == "":
+            raise ValueError("password is empty")
+        self.__password = connection_params['password']
         self.org = connection_params['organization']
         self.bucket = connection_params['bucket']
         # TODO create Token from username and password
-        token = self.username + "" + self.password
+        token = self.__username + " " + self.__password
         self.influxDBClient = InfluxDBClient(url=self.url, token=token,
                                              org=self.org)
         self.write_api = self.influxDBClient.write_api(
@@ -21,14 +28,6 @@ class InfluxWrapper:  # renamed so not as imported class
     @property
     def url(self):
         return self._url
-
-    @property
-    def username(self):
-        return self._username
-
-    @property
-    def password(self):
-        return self.__password
 
     @property
     def org(self):
@@ -47,22 +46,6 @@ class InfluxWrapper:  # renamed so not as imported class
             raise ValueError("url is empty")
         self._url = url
 
-    @username.setter
-    def username(self, username):
-        if username is None:
-            raise ValueError("variable value of username is none")
-        if username == "":
-            raise ValueError("username is empty")
-        self._username = username
-
-    @password.setter
-    def password(self, password):
-        if password is None:
-            raise ValueError("variable value of password is none")
-        if password == "":
-            raise ValueError("password is empty")
-        self.__password = password
-
     @org.setter
     def org(self, org):
         if org is None:
@@ -79,7 +62,6 @@ class InfluxWrapper:  # renamed so not as imported class
             raise ValueError("bucket is empty")
         self._bucket = bucket
 
-    # TODO check if written successfully
     def insert(self, point):
         try:
             with self.influxDBClient.write_api() as _write_client:
@@ -89,7 +71,6 @@ class InfluxWrapper:  # renamed so not as imported class
             # urllib.error.URLError
             return 1
 
-    # TODO check if written successfully
     def insert_many(self, points):
         try:
             with self.influxDBClient.write_api(write_options=WriteOptions(
