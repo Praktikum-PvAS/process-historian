967ed57a4dab7f8d0cd3913849fcce3333932071
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-06-15 12:26:21 +0200

    Add stub code for configurator.py

--- src/configBuilder/configurator.py
+++ src/configBuilder/configurator.py
@@ -1,4 +1,17 @@
+import json
+
+from sparqlClient import SPARQLClient
+
 
 class Configurator:
     def __init__(self, connection_params: dict):
-        self.connection_parameters = connection_params
+        self.connection_params = connection_params
+        self.sparql_client = SPARQLClient(connection_params)
+
+    def write_config(self):
+        nodes = self.sparql_client.get_all_nodes()
+        with open("../config/opcua_config.json", "w") as file:
+            json.dump(nodes, file)
+
+    def on_exit(self):
+        self.sparql_client.disconnect()
