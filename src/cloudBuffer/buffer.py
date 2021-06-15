ffd13897f2e843bbfdec4b726e783310810e97d8
Oliver Parczyk <oliver.parczyk@mailbox.tu-dresden.de>
2021-06-15 12:21:25 +0200

    Add type hinting

--- src/cloudBuffer/buffer.py
+++ src/cloudBuffer/buffer.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from influxdb_client import Point
 from .influxWrapper import InfluxWrapper
 
@@ -7,7 +9,7 @@ class Buffer:
         self.__buffer = []
         self.influxClient = InfluxWrapper(connection_params)
 
-    def append(self, node_name, value, timestamp):
+    def append(self, node_name: str, value: Any, timestamp: Any):
         # TODO structure?
         # TODO check buffersize
         self.__buffer.append(
@@ -18,19 +20,19 @@ class Buffer:
     def write_points(self):
         if not len(self.__buffer) == 1:
             status = self.influxClient.insert(self.__buffer[0])
-            if not status:      # successful
+            if not status:  # successful
                 self.__buffer.remove(0)
         elif len(self.__buffer) > 1000:
             __buffer_part = self.__buffer[:1000].copy()
             status = self.influxClient.insert_many(__buffer_part)
-            if not status:      # successful
+            if not status:  # successful
                 self.pop_first(len(__buffer_part))
-            else:               # push back when not written
+            else:  # push back when not written
                 self.__buffer.insert(0, __buffer_part)
         else:
             status = self.influxClient.insert_many(self.__buffer)
             if not status:  # successful
                 self.pop_first(len(self.__buffer))
 
-    def pop_first(self, number_of_elements):
+    def pop_first(self, number_of_elements: int):
         self.__buffer[0:number_of_elements] = []
