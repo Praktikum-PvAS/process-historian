3422eeab56d1682f73e4fe61aa4352c94913146a
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-06-15 12:26:09 +0200

    Add stub code for sparqlClient.py

--- src/configBuilder/sparqlClient.py
+++ src/configBuilder/sparqlClient.py
@@ -1,8 +1,24 @@
 import sparql
-# TODO which sparql to import?
 
 
 class SPARQLClient:
     def __init__(self, connection_params: dict):
+        self.connection_params = connection_params
+        self.connect(connection_params)
+
+    def connect(self, connection_params):
         # TODO
+        pass
+
+    def disconnect(self):
+        # TODO
+        pass
+
+    def get_all_nodes(self):
+        # TODO
+        # - find opc ua host
+        # - find sensors, actuators, services
+        # - for all find attributes
+        # - from attributes get nodeId and namespace
+        # - return dict
         pass
