e32fb9478be22bc19d6ab55a200dabf1caf173e7
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-06-16 18:34:40 +0200

    Add opcua config location and included types as constructor params

--- src/configBuilder/configurator.py
+++ src/configBuilder/configurator.py
@@ -4,12 +4,14 @@ from .sparqlClient import SPARQLClient
 
 
 class Configurator:
-    def __init__(self, connection_params: dict):
-        self.__sparql_client = SPARQLClient(connection_params)
+    def __init__(self, connection_params: dict, included_types: list,
+                 config_loc: str):
+        self.__config_loc = config_loc
+        self.__sparql_client = SPARQLClient(connection_params, included_types)
 
     def write_config(self):
         nodes = self.__sparql_client.get_all_nodes()
-        with open("../config/opcua_config.json", "w") as file:
+        with open(self.__config_loc, "w") as file:
             json.dump(nodes, file)
 
     def on_exit(self):
