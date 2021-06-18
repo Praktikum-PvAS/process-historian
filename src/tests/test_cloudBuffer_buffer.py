1a7c1febee7cc99b14703855679015f67fa3eb93
Kirchner, Max <kirchnermax@mailbox.tu-dresden.de>
2021-06-18 18:29:20 +0300

    added test cloudBuffer and test influxWrapper

--- src/tests/test_cloudBuffer_buffer.py
+++ src/tests/test_cloudBuffer_buffer.py
@@ -1,5 +1,9 @@
 import unittest
+
+from influxdb_client import Point
+
 from cloudBuffer.buffer import Buffer
+
 
 # TODO Necessary to insert a test function for each case? (None, int, Str, ...)?
 # TODO What to add to avoid bare except?
@@ -10,76 +14,85 @@ class MyTestCase(unittest.TestCase):
         # try catch
         # https://github.com/influxdata/influxdb-client-python/blob/0c1d1d9ff92dd2b3b4a9b6aa1e8f5b1c02fd48ab/influxdb_client/client/write/point.py#L80
         # possible Timestamps
-        try:
-            my_connection_params = {
-                "host": "localhost",
-                "username": "root",
-                "password": "root",
-                "organization": "TUD",
-                "bucket": "",
-                "port": 8086
-            }
-            my_buffer = Buffer(my_connection_params)
-            my_buffer.append("node_name", 1, "2009-11-10T23:00:00.123456Z")
-        except:
-            self.fail("invalid insert")
+        my_connection_params = {
+            "host": "localhost",
+            "username": "root",
+            "password": "root",
+            "organization": "TUD",
+            "bucket": "",
+            "port": 8086
+        }
+
+        test_point = Point("node_name")
+        test_point.tag("tag_a", "tag_b")
+        test_point.field("value", 1)
+        test_point.time("2009-11-10T23:00:00.123456Z")
+
+        my_buffer = Buffer(my_connection_params)
+        my_buffer.append("node_name", [("tag_a", "tag_b")], 1, "2009-11-10T23:00:00.123456Z")
+        self.assertEquals(my_buffer._Buffer__buffer[-1], test_point)
 
     def test_append_invalid_node_name_insert(self):
-        try:
-            my_connection_params = {
-                "host": "localhost",
-                "username": "root",
-                "password": "root",
-                "organization": "TUD",
-                "bucket": "",
-                "port": 8086
-            }
-            my_buffer = Buffer(my_connection_params)
-            my_buffer.append("", 1, "2009-11-10T23:00:00.123456Z")
-            my_buffer.append(None, 1, "2009-11-10T23:00:00.123456Z")
-            my_buffer.append(1, 1, "2009-11-10T23:00:00.123456Z")
-        except:
-            self.fail("invalid insert")
+        my_connection_params = {
+            "host": "localhost",
+            "username": "root",
+            "password": "root",
+            "organization": "TUD",
+            "bucket": "",
+            "port": 8086
+        }
+        my_buffer = Buffer(my_connection_params)
+        with self.assertRaises(ValueError):
+            my_buffer.append("", [("tag_a", "tag_b")], 1, "2009-11-10T23:00:00.123456Z")
+        with self.assertRaises(ValueError):
+            my_buffer.append(None, [("tag_a", "tag_b")], 1, "2009-11-10T23:00:00.123456Z")
+
+    def test_append_invalid_tag_insert(self):
+        my_connection_params = {
+            "host": "localhost",
+            "username": "root",
+            "password": "root",
+            "organization": "TUD",
+            "bucket": "",
+            "port": 8086
+        }
+        my_buffer = Buffer(my_connection_params)
+        with self.assertRaises(ValueError):
+            my_buffer.append("node_name", None, 1, "2009-11-10T23:00:00.123456Z")
+        with self.assertRaises(ValueError):
+            my_buffer.append("node_name", [], 1, "2009-11-10T23:00:00.123456Z")
 
     def test_append_invalid_value_insert(self):
-        try:
-            my_connection_params = {
-                "host": "localhost",
-                "username": "root",
-                "password": "root",
-                "organization": "TUD",
-                "bucket": "",
-                "port": 8086
-            }
-            my_buffer = Buffer(my_connection_params)
-            my_buffer.append("node_name", None, "2009-11-10T23:00:00.123456Z")
-            my_buffer.append("node_name", "123", "2009-11-10T23:00:00.123456Z")
-        except:
-            self.fail("invalid insert")
+        my_connection_params = {
+            "host": "localhost",
+            "username": "root",
+            "password": "root",
+            "organization": "TUD",
+            "bucket": "",
+            "port": 8086
+        }
+        my_buffer = Buffer(my_connection_params)
+        with self.assertRaises(ValueError):
+            my_buffer.append("node_name", [("tag_a", "tag_b")], [], "2009-11-10T23:00:00.123456Z")
 
     def test_append_invalid_timestamp_insert(self):
-        try:
-            my_connection_params = {
-                "host": "localhost",
-                "username": "root",
-                "password": "root",
-                "organization": "TUD",
-                "bucket": "",
-                "port": 8086
-            }
-            my_buffer = Buffer(my_connection_params)
-            my_buffer.append("node_name", 1, None)
-            my_buffer.append("node_name", 1, "")
-        except:
-            self.fail("invalid insert")
+        my_connection_params = {
+            "host": "localhost",
+            "username": "root",
+            "password": "root",
+            "organization": "TUD",
+            "bucket": "",
+            "port": 8086
+        }
+        my_buffer = Buffer(my_connection_params)
+        with self.assertRaises(ValueError):
+            my_buffer.append("node_name", [("tag_a", "tag_b")], 1, None)
 
     def test_valid_write_points(self):
         my_buffer = Buffer()
         my_buffer.append("node_name", 1, "2009-11-10T23:00:00.123456Z")
-        try:
+        with self.assertRaises():
             my_buffer.write_points()
-        except:
-            self.fail("point couldn't write point")
 
     # TODO How to show invalid test?
     def test_invalid_write_points(self):
--- src/tests/test_cloudBuffer_influxWrapper.py
+++ src/tests/test_cloudBuffer_influxWrapper.py
@@ -1,9 +1,49 @@
 import unittest
 
+from cloudBuffer.influxWrapper import InfluxWrapper
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_insert_many(self):
+        my_connection_params = {
+            "host": "localhost",
+            "username": "root",
+            "password": "root",
+            "organization": "TUD",
+            "bucket": "A",
+            "port": 8086
+        }
+        my_influx_wrapper = InfluxWrapper(my_connection_params)
+        with self.assertRaises(ValueError):
+            my_influx_wrapper.insert_many(None)
+
+    # TODO ValueError not raised
+    def test_insert_many_2(self):
+        my_connection_params = {
+            "host": "localhost",
+            "username": "root",
+            "password": "root",
+            "organization": "TUD",
+            "bucket": "A",
+            "port": 8086
+        }
+        my_influx_wrapper = InfluxWrapper(my_connection_params)
+        with self.assertRaises(ValueError):
+            points = []
+            my_influx_wrapper.insert_many(points)
+
+    def test_insert(self):
+        my_connection_params = {
+            "host": "localhost",
+            "username": "root",
+            "password": "root",
+            "organization": "TUD",
+            "bucket": "A",
+            "port": 8086
+        }
+        my_influx_wrapper = InfluxWrapper(my_connection_params)
+        with self.assertRaises(ValueError):
+            my_influx_wrapper.insert(None)
 
 
 if __name__ == '__main__':
