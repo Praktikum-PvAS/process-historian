e55ecffd7a10959eb25f22bac94b160dcf0e9305
dohlenaugur <45801766+dohlenaugur@users.noreply.github.com>
2021-06-29 11:29:19 +0200

    Added value error for unexpected assembly type

--- src/opcuaClient/opcuaClient.py
+++ src/opcuaClient/opcuaClient.py
@@ -11,6 +11,10 @@ class CustomNode:
     def __init__(self, assembly_type: str, assembly_identifier: str,
                  attribute_name: str,
                  opc_node: opcua.Node):
+        type_list = ["sensors", "actuators", "services"]
+        if not(assembly_type in type_list):
+            raise ValueError("Unexpected assembly type. Expected are only sensors, actuators or services!")
+
         self.assembly_type = assembly_type
         self.assembly_identifier = assembly_identifier
         self.attribute_name = attribute_name
