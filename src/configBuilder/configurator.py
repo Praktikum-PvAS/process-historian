a98d98994b8c841ea79320ffc7f2a479559c9b2d
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-07-06 14:53:38 +0200

    Add docstrings

--- src/configBuilder/configurator.py
+++ src/configBuilder/configurator.py
@@ -4,17 +4,35 @@ from .sparqlClient import SPARQLClient
 
 
 class Configurator:
+    """
+    Configures the connection to the TripleStore and writes the
+    OPC UA config
+    """
+
     def __init__(self, connection_params: dict, included_types: list,
                  config_loc: str):
+        """
+        Constructor
+        :param connection_params: Connection params for the SPARQLClient
+        :param included_types: List of included DataAssembly types
+        :param config_loc: Path where the OPC UA config should be written to
+        """
         self.__config_loc = config_loc
         self.__sparql_client = SPARQLClient(connection_params, included_types)
 
     def write_config(self):
+        """
+        Fetches the data from TripleStore and writes the config
+        """
         nodes = self.__sparql_client.get_all_nodes()
         with open(self.__config_loc, "w") as file:
             json.dump(nodes, file, indent=2)
 
     def write_debug_config(self, exists: bool):
+        """
+        Writes a debug config if no other config is already in place.
+        :param exists: Config already exists
+        """
         if exists:
             return
         else:
@@ -27,4 +45,7 @@ class Configurator:
                 }, file, indent=2)
 
     def on_exit(self):
+        """
+        Disconnects the SPARQLClient
+        """
         self.__sparql_client.disconnect()
--- src/configBuilder/sparqlClient.py
+++ src/configBuilder/sparqlClient.py
@@ -2,7 +2,16 @@ from SPARQLWrapper import SPARQLWrapper
 
 
 class SPARQLClient:
+    """
+    Client to make SPARQL requests to the TripleStore
+    """
+
     def __init__(self, connection_params: dict, included_types: list):
+        """
+        Constructor
+        :param connection_params: Connection params to the TripleStore
+        :param included_types: List of included DataAssembly types
+        """
         if connection_params is None:
             raise ValueError("connection_params must not be None")
         if "host" not in connection_params \
@@ -22,15 +31,26 @@ class SPARQLClient:
         self.__included_types = included_types
         self.__sparql = SPARQLWrapper(connection_params["host"])
 
-    def connect(self, connection_params):
+    def connect(self):
+        """
+        Currently does nothing. Should connect the client to the TripleStore.
+        """
         # TODO
         pass
 
     def disconnect(self):
+        """
+        Currently does nothing. Should disconnect from the TripleStore.
+        """
         # TODO
         pass
 
-    def get_all_nodes(self):
+    def get_all_nodes(self) -> dict:
+        """
+        Currently does nothing. Should fetch all necessary data from the
+        TripleStore and make an organized dict out of it.
+        :return: Dict in schema of the opcua_config_schema.json
+        """
         # TODO
         # - find opc ua host
         # - find sensors, actuators, services (depends on "included_types")
