0be5662d1852238c345d7db4cec87af915916142
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-07-13 12:56:33 +0200

    Remove licence header
    This test is not based on Valentins tests in any way anymore. It was rewritten from ground up.

--- src/tests/test_integration_opcuaClient.py
+++ src/tests/test_integration_opcuaClient.py
@@ -1,33 +1,3 @@
-#######################################################################################
-#    Title: test_opcua_listener source code
-#    Author: Valentin Khaydarov
-#    Date: 23.01.2021
-#    Code version: 511435a
-#    Availability: https://github.com/Praktikum-PvAS/planteye-nebula/blob/57616be37af3aee143826c7fca484592c4c27e65/tests/test_opcua_listener.py
-#
-#    MIT License
-#
-#    Copyright (c) 2021 Valentin Khaydarov
-#    Copyright (c) 2021 Max Kirchner, Patrick Suwinski
-#
-#    Permission is hereby granted, free of charge, to any person obtaining a copy
-#    of this software and associated documentation files (the "Software"), to deal
-#    in the Software without restriction, including without limitation the rights
-#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#    copies of the Software, and to permit persons to whom the Software is
-#    furnished to do so, subject to the following conditions:
-#
-#    The above copyright notice and this permission notice shall be included in all
-#    copies or substantial portions of the Software.
-#
-#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#    SOFTWARE.
-#######################################################################################
 import time
 import unittest
 import threading
