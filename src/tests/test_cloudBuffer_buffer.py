ef02571f0cdf6576150cb8865877e14218369d25
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-07-13 10:22:52 +0200

    Rework buffer tests

--- src/tests/test_cloudBuffer_buffer.py
+++ src/tests/test_cloudBuffer_buffer.py
@@ -6,78 +6,105 @@ from cloudBuffer.buffer import Buffer
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.valid_connection_params = {
+            "host": "localhost",
+            "organization": "TUD",
+            "bucket": "my_bucket",
+            "port": 8086,
+            "token": "my_token"
+        }
+        self.test_buffer = Buffer(1000, self.valid_connection_params)
+
+    def test_constructor(self):
+        with self.assertRaises(ValueError):
+            buffer = Buffer(None, self.valid_connection_params)
+        with self.assertRaises(ValueError):
+            buffer = Buffer(0, self.valid_connection_params)
+        with self.assertRaises(ValueError):
+            buffer = Buffer(-2, self.valid_connection_params)
+
+        buffer = Buffer(1, self.valid_connection_params)
+
     def test_append_valid_insert(self):
-        # try catch
         # https://github.com/influxdata/influxdb-client-python/blob/0c1d1d9ff92dd2b3b4a9b6aa1e8f5b1c02fd48ab/influxdb_client/client/write/point.py#L80
         # possible Timestamps
-        my_connection_params = {
-            "host": "localhost",
-            "organization": "TUD",
-            "bucket": "my_bucket",
-            "port": 8086,
-            "token": "my_token"
-        }
-
-        test_point = Point("node_name")
-        test_point.tag("tag_a", "tag_b")
-        test_point.field("value", 1)
-        test_point.time("2009-11-10T23:00:00.123456Z")
-
-        my_buffer = Buffer(1000, my_connection_params)
-        my_buffer.append("node_name", [("tag_a", "tag_b")], 1, "2009-11-10T23:00:00.123456Z")
-        self.assertEquals(my_buffer._Buffer__buffer[-1], test_point)
+        self.test_buffer.append("node_name", [("tag_a", "tag")],
+                                [("value", 1)], "2009-11-10T23:00:00.123456Z")
+        self.assertEqual(len(self.test_buffer._Buffer__buffer), 1)
 
     def test_append_invalid_node_name_insert(self):
-        my_connection_params = {
-            "host": "localhost",
-            "organization": "TUD",
-            "bucket": "my_bucket",
-            "port": 8086,
-            "token": "my_token"
-        }
-        my_buffer = Buffer(1000, my_connection_params)
         with self.assertRaises(ValueError):
-            my_buffer.append("", [("tag_a", "tag_b")], 1, "2009-11-10T23:00:00.123456Z")
+            self.test_buffer.append("", [("tag_a", "tag")], [("value", 1)],
+                                    "2009-11-10T23:00:00.123456Z")
         with self.assertRaises(ValueError):
-            my_buffer.append(None, [("tag_a", "tag_b")], 1, "2009-11-10T23:00:00.123456Z")
-
-    # def test_append_invalid_tag_insert(self):
-    #    my_connection_params = {
-    #        "host": "localhost",
-    #        "username": "root",
-    #        "password": "root",
-    #        "organization": "TUD",
-    #        "bucket": "my_bucket",
-    #        "port": 8086,
-    #        "token": "my_token"
-    #    }
-    #    my_buffer = Buffer(1000, my_connection_params)
-    #    with self.assertRaises(ValueError):
-    #        my_buffer.append("node_name", None, 1, "2009-11-10T23:00:00.123456Z")
+            self.test_buffer.append(None, [("tag_a", "tag")],
+                                    [("value", 1)],
+                                    "2009-11-10T23:00:00.123456Z")
 
     def test_append_invalid_value_insert(self):
-        my_connection_params = {
-            "host": "localhost",
-            "organization": "TUD",
-            "bucket": "my_bucket",
-            "port": 8086,
-            "token": "my_token"
-        }
-        my_buffer = Buffer(1000, my_connection_params)
         with self.assertRaises(ValueError):
-            my_buffer.append("node_name", [("tag_a", "tag_b")], None, "2009-11-10T23:00:00.123456Z")
+            self.test_buffer.append("node_name", [("tag_a", "tag")], None,
+                                    "2009-11-10T23:00:00.123456Z")
+        with self.assertRaises(ValueError):
+            self.test_buffer.append("node_name", [("tag_a", "tag")], [],
+                                    "2009-11-10T23:00:00.123456Z")
 
     def test_append_invalid_timestamp_insert(self):
-        my_connection_params = {
-            "host": "localhost",
-            "organization": "TUD",
-            "bucket": "my_bucket",
-            "port": 8086,
-            "token": "my_token"
-        }
-        my_buffer = Buffer(1000, my_connection_params)
         with self.assertRaises(ValueError):
-            my_buffer.append("node_name", [("tag_a", "tag_b")], 1, None)
+            self.test_buffer.append("node_name", [("tag_a", "tag")],
+                                    [("value", 1)], None)
+
+    def test_append_many(self):
+        points = []
+        for i in range(10):
+            points.append(("node_name", [("tag_a", "tag")], [("value", 1)],
+                           "2009-11-10T23:00:00.123456Z"))
+        self.test_buffer.append_many(points)
+        self.assertEqual(len(self.test_buffer._Buffer__buffer), 10)
+
+    def test_append_many_invalid(self):
+        # Name
+        points = [("", [("tag_a", "tag")], [("value", 1)],
+                   "2009-11-10T23:00:00.123456Z")]
+        with self.assertRaises(ValueError):
+            self.test_buffer.append_many(points)
+        points = [(None, [("tag_a", "tag")], [("value", 1)],
+                   "2009-11-10T23:00:00.123456Z")]
+        with self.assertRaises(ValueError):
+            self.test_buffer.append_many(points)
+        # Value
+        points = [("node_name", [("tag_a", "tag")], [],
+                   "2009-11-10T23:00:00.123456Z")]
+        with self.assertRaises(ValueError):
+            self.test_buffer.append_many(points)
+        points = [("node_name", [("tag_a", "tag")], None,
+                   "2009-11-10T23:00:00.123456Z")]
+        with self.assertRaises(ValueError):
+            self.test_buffer.append_many(points)
+        # Timestamp
+        points = [("node_name", [("tag_a", "tag")], [("value", 1)], None)]
+        with self.assertRaises(ValueError):
+            self.test_buffer.append_many(points)
+
+    def test_max_buffer_length(self):
+        points = []
+        for i in range(1050):
+            points.append(("node_name", [("tag_a", "tag")], [("value", 1)],
+                           "2009-11-10T23:00:00.123456Z"))
+        self.test_buffer.append_many(points)
+        self.assertEqual(len(self.test_buffer._Buffer__buffer), 1000)
+
+        self.test_buffer.append("node_name", [("tag_a", "tag")],
+                                [("value", 1)], "2009-11-10T23:00:00.123456Z")
+        self.assertEqual(len(self.test_buffer._Buffer__buffer), 1000)
+
+        points = []
+        for i in range(10):
+            points.append(("node_name", [("tag_a", "tag")], [("value", 1)],
+                           "2009-11-10T23:00:00.123456Z"))
+        self.test_buffer.append_many(points)
+        self.assertEqual(len(self.test_buffer._Buffer__buffer), 1000)
 
 
 if __name__ == '__main__':
